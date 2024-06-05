fc3f52592ba6a1653440896ee32c525b99e44532
leenabaxioned <leenab@axioned.com>
2024-06-05 13:33:37 +0530

    script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
 /* Author: */
+var jokes = document.querySelector(".jokes");
+var btn = document.querySelector("#get");
 
-
+var generateJokes = async () => {
+  try {
+    var setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    // fetching Api with async await
+    var url = "https://icanhazdadjoke.com";
+    var res = await fetch(url, setHeader);
+    var data = await res.json();
+    console.log(data);
+    jokes.innerHTML = data.joke;
+  } catch (error) {
+    console.log(`The error is ${error}`);
+  }
+};
+// calling a function
+btn.addEventListener("click", generateJokes);
+//generateJokes();
